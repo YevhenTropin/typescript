33a809bd36fb84b330e17f07ec948fb1b577fcc9
evgeniy-tropin <evgeniy.tropin@flexaspect.com>
2025-01-13 16:02:46 +0200

    HW 4.1

--- main.ts
+++ main.ts
@@ -2,12 +2,12 @@ interface IBookService {
     books: IBook[]
     authors: IAuthor[]
     getBooks(): IBook[]
-    getBookById(bookId : number): IBook | undefined
+    getBookById(bookId: number): IBook | undefined
     getAuthors(): IAuthor[]
-    getAuthorById(authorId : number): IAuthor | undefined
-    getBooksByAuthor(authorParam : string | number): IBook[]
-    getAuthorByBookId(id : number): IAuthor
-    search(searchParam : string | number): IBook[]
+    getAuthorById(authorId: number): IAuthor | undefined
+    getBooksByAuthor(authorParam: string | number): IBook[]
+    getAuthorByBookId(id: number): IAuthor
+    search(searchParam: string | number): IBook[]
 }
 
 interface IBook {
@@ -38,53 +38,53 @@ class BookService implements IBookService {
         { authorId: 3, name: "Ernest Miller Hemingway", birthday: 1899 },
     ]
 
-    getBooks() : IBook[] | undefined {
+    getBooks(): IBook[] | undefined {
         return this.books
     }
 
-    getBookById(bookId : number) : IBook | undefined {
-        return this.books.find((book: IBook) : boolean => book.bookId === bookId)
+    getBookById(bookId: number): IBook | undefined {
+        return this.books.find((book: IBook): boolean => book.bookId === bookId)
     }
 
-    getAuthors() : IAuthor[] | undefined {
+    getAuthors(): IAuthor[] | undefined {
         return this.authors
     }
 
-    getAuthorById(authorId : number) : IAuthor | undefined {
-        return this.authors.find((author: IAuthor) : boolean => author.authorId === authorId)
+    getAuthorById(authorId: number): IAuthor | undefined {
+        return this.authors.find((author: IAuthor): boolean => author.authorId === authorId)
     }
 
-    getBooksByAuthor(authorParam : string | number) : IBook[] | undefined {
+    getBooksByAuthor(authorParam: string | number): IBook[] | undefined {
         if (typeof authorParam === "number") {
-            return this.books.filter((book : IBook) : boolean => book.authorId === authorParam)
+            return this.books.filter((book: IBook): boolean => book.authorId === authorParam)
         } else if (typeof authorParam === "string") {
-            return this.books.filter((book : IBook) : boolean => {
-                const author : IAuthor = this.authors.find((author : IAuthor) : boolean => author.name === authorParam)
+            return this.books.filter((book: IBook): boolean => {
+                const author: IAuthor = this.authors.find((author: IAuthor): boolean => author.name === authorParam)
                 return author ? book.authorId === author.authorId : false
             });
         }
         return [];
     }
 
-    getAuthorByBookId(bookId : number) : IAuthor | undefined {
-        const book : IBook = this.books.find((book : IBook) : boolean => book.bookId === bookId);
-        return book ? this.authors.find((author: IAuthor) : boolean => author.authorId === book.authorId) : undefined;
+    getAuthorByBookId(bookId: number): IAuthor | undefined {
+        const book: IBook = this.books.find((book: IBook): boolean => book.bookId === bookId)
+        return book ? this.authors.find((author: IAuthor): boolean => author.authorId === book.authorId) : undefined
     }
 
-    search(searchParam : string | number) : IBook[] {
+    search(searchParam: string | number): IBook[] {
         if (typeof searchParam === "number") {
-            return this.books.filter((book : IBook) : boolean => book.year === searchParam)
+            return this.books.filter((book: IBook): boolean => book.year === searchParam)
         }
 
-        const author : IAuthor = this.authors.find((author : IAuthor) : boolean => author.name === searchParam)
+        const author: IAuthor = this.authors.find((author: IAuthor): boolean => author.name === searchParam)
 
         if (author) {
-            return this.books.filter((book : IBook) : boolean => {
+            return this.books.filter((book: IBook): boolean => {
                 return book.authorId === author.authorId
             });
         }
 
-        return this.books.filter((book : IBook) : boolean => {
+        return this.books.filter((book: IBook): boolean => {
             return book.title.includes(searchParam) ||
                 book.genre.includes(searchParam)
         });
@@ -92,7 +92,7 @@ class BookService implements IBookService {
 }
 
 class Book implements IBook {
-    static nextId = 1;
+    static nextId = 1
 
     bookId: number
     title: string
@@ -100,35 +100,35 @@ class Book implements IBook {
     year: number
     authorId: number
 
-    constructor(title : string, genre : string, year : number, authorId : number) {
-        this.bookId = Book.nextId++;
-        this.title = title;
-        this.genre = genre;
-        this.year = year;
-        this.authorId = authorId;
+    constructor(title: string, genre: string, year: number, authorId: number) {
+        this.bookId = Book.nextId++
+        this.title = title
+        this.genre = genre
+        this.year = year
+        this.authorId = authorId
     }
 }
 
 class Author implements IAuthor {
-    static nextId = 1;
+    static nextId = 1
 
     authorId: number
     name: string
     birthday: number
 
-    constructor(name : string, birthday : number) {
-        this.authorId = Author.nextId++;
-        this.name = name;
-        this.birthday = birthday;
+    constructor(name: string, birthday: number) {
+        this.authorId = Author.nextId++
+        this.name = name
+        this.birthday = birthday
     }
 }
 
-const bookService = new BookService();
+const bookService = new BookService()
 
-console.log(bookService.search("Novel"));
+console.log(bookService.search("Novel"))
 
-console.log(bookService.getBooks());
+console.log(bookService.getBooks())
 
-console.log(bookService.getBooksByAuthor("Daniel Keyes"));
+console.log(bookService.getBooksByAuthor("Daniel Keyes"))
 
-console.log(bookService.getAuthorByBookId(2));
+console.log(bookService.getAuthorByBookId(2))
