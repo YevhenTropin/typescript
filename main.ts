d432c78242485f7226221f89b26d839f8b9707b4
evgeniy-tropin <evgeniy.tropin@flexaspect.com>
2025-01-20 11:17:56 +0200

    HW 7.2

--- main.ts
+++ main.ts
@@ -13,6 +13,16 @@ function fetchData(): unknown {
 const personData: IData = fetchData() as IData
 
 function printPersonInfo(person: IData): asserts person is IData {
+    if (typeof person !== 'object' || person === null) {
+        throw new Error("The person is not an object.")
+    }
+    if (typeof person.name !== 'string') {
+        throw new Error("The 'name' property must be a string.")
+    }
+    if (typeof person.age !== 'number') {
+        throw new Error("The 'age' property must be a number.")
+    }
+
     console.log(`Name: ${person.name}`)
     console.log(`Age: ${person.age}`)
 }
